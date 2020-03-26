90a0d465fc7f91edb09364097ac4c9b2c8b14e86
1devhall <devoughnh@gmail.com>
2020-03-26 19:45:12 -0400

    mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, useHistory } from 'react-router-dom';
 import MovieCard from './MovieCard';
 
-function Movie({ addToSavedList }) {
+function Movie({ addToSavedList, decreaseMovieCount }) {
   const [movie, setMovie] = useState(null);
   const match = useRouteMatch();
+  const history = useHistory();
 
   const fetchMovie = id => {
     axios
@@ -17,22 +18,37 @@ function Movie({ addToSavedList }) {
   const saveMovie = () => {
     addToSavedList(movie);
   };
+  const editMovie = () => {
+    history.push(`/update-movie/${match.params.id}`);
+  }
+
+  const deleteMovie = () => {
+    const id = match.params.id;
+    axios.delete(`http://localhost:5000/api/movies/${id}`)
+    .then(() => {
+      decreaseMovieCount();
+      history.push(`/`);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
 
   useEffect(() => {
     fetchMovie(match.params.id);
   }, [match.params.id]);
 
   if (!movie) {
-    return <div>Loading movie information...</div>;
+    return <div>Loading info...</div>;
   }
 
   return (
     <div className='save-wrapper'>
       <MovieCard movie={movie} />
 
-      <div className='save-button' onClick={saveMovie}>
-        Save
-      </div>
+      <div className='delete-button' onClick={deleteMovie}>Delete Movie</div>
+      <div className='save-button' onClick={saveMovie}>Save Movie</div>
+      <div className='edit-button' onClick={editMovie}>Edit Movie</div>
     </div>
   );
 }
